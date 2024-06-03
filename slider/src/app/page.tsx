23257a0dbb9711b58721a37541ee4010ad53a27d
pandeeswari1926 <pandeeswariashok@gmail.com>
2024-06-03 18:29:14 +0530

    slider 2 & 3 added

--- slider/src/app/page.tsx
+++ slider/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Page from "./home/page";
+import Page from "./service/Page";
 
 export default function Home() {
   return (
--- slider/src/app/testimonial/Page.tsx
+++ slider/src/app/testimonial/Page.tsx
@@ -17,47 +17,45 @@ const Page = () => {
   };
   return (
     <>
-      <Slider {...settings} className="lg:h-screen h-[550px]">
+      <div>
+        <Slider {...settings}>
         <div className="relative lg:h-screen h-[550px] w-full rounded-lg bg-cover bg-center flex items-center justify-center">
           <img
-            src="/banner.webp"
+            src="banner3.avif"
             alt=""
             className="absolute w-full h-full object-cover"
           />
-          <div className="absolute w-full h-full bg-black bg-opacity-75 lg:text-start text-center lg:gap-0 gap-5 flex-col-reverse flex lg:flex-row items-center justify-center ">
-            <div className=" lg:w-1/2 md:pl-10 space-y-7">
+          <div className="absolute w-full h-full bg-black bg-opacity-75 text-center lg:gap-0 gap-5 flex flex-col justify-center items-center ">
+            <div className="space-y-7">
               <h1 className="text-white font-bold text-sm">For Millions of Users</h1>
               <h1 className="text-white  md:text-5xl text-xl font-bold">
-                <span className="sha">Embracing Technology</span> Empowering Business
+                <span className="sha">Transforming Lives, </span> One Smile at a Time
               </h1>
               <button className="bg-white p-3 px-8 rounded-md w-fit hover:bg-primary duration-500 hover:text-white">Get a Quote</button>
-            </div>
-            <div className="lg:w-1/2 lg:block  px-5">
-                <img src="image2.png" alt="" className="w-full h-full"/>
             </div>
           </div>
 
         </div>
         <div className="relative lg:h-screen h-[550px] w-full rounded-lg bg-cover bg-center flex items-center justify-center">
           <img
-            src="/banner.webp"
+            src="banner3.avif"
             alt=""
             className="absolute w-full h-full object-cover"
           />
-          <div className="absolute w-full h-full bg-black bg-opacity-75 lg:text-start text-center flex lg:gap-0 gap-5 flex-col-reverse lg:flex-row items-center justify-center ">
-            <div className=" lg:w-1/2 md:pl-10 space-y-7">
+          <div className="absolute w-full h-full bg-black bg-opacity-75 text-center lg:gap-0 gap-5 flex flex-col justify-center items-center ">
+            <div className="space-y-7">
+              <h1 className="text-white font-bold text-sm">For Millions of Users</h1>
               <h1 className="text-white  md:text-5xl text-xl font-bold">
-                <span className="sha">READY TO BUILD YOUR </span> WEB PRESENCE?
+                <span className="sha">At the heart of everything we do,</span> 
               </h1>
-              <p className="text-white text-sm font-bold leading-loose">GET A HEAD START WITH FREE DOMAIN & HOSTING LIMITED TIME OFFER, DON'T MISS OUT!</p>
+              <h1 className="text-white  md:text-5xl text-xl font-bold">See how we've made a difference..</h1>
               <button className="bg-white p-3 px-8 rounded-md w-fit hover:bg-primary duration-500 hover:text-white">Get a Quote</button>
             </div>
-            <div className="lg:w-1/2 lg:block px-5">
-                <img src="/image1.png" alt="" className="w-full h-full"/>
-            </div>
           </div>
+
         </div>
-      </Slider>
+        </Slider>
+      </div>
     </>
   );
 };
